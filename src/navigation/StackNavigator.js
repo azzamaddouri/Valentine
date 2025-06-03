bbf1ef35655469b8f260dad7ef4ae95369d429d5
azzamaddouri <maddouriazza@gmail.com>
2025-06-04 00:22:34 +0100

    Add authentication screens and integrate Lottie animations

--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -11,6 +11,22 @@ import Ionicons from 'react-native-vector-icons/Ionicons'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import { NavigationContainer } from '@react-navigation/native'
+import LoginScreen from '@features/auth/login/LoginScreen'
+import BasicInfo from '@features/auth/register/BasicInfo'
+import NameScreen from '@features/auth/register/NameScreen'
+import EmailScreen from '@features/auth/register/EmailScreen'
+import GenderScreen from '@features/auth/register/GenderScreen'
+import LocationScreen from '@features/auth/register/LocationScreen'
+import PasswordScreen from '@features/auth/register/PasswordScreen'
+import TypeScreen from '@features/auth/register/TypeScreen'
+import BirthScreen from '@features/auth/register/BirthScreen'
+import DatingType from '@features/auth/register/DatingType'
+import LookingFor from '@features/auth/register/LookingFor'
+import PreFinalScreen from '@features/auth/register/PreFinalScreen'
+import ShowPromptsScreen from '@features/auth/register/ShowPromptsScreen'
+import HomeTownScreen from '@features/auth/register/HomeTownScreen'
+import  PhotoScreen from '@features/auth/register/PhotoScreen'
+import PromptsScreen from '@features/auth/register/PromptsScreen'
 
 
 const StackNavigator = () => {
@@ -108,6 +124,92 @@ const StackNavigator = () => {
   }
 
 
+  const AuthStack = () => (
+    <Stack.Navigator>
+      {/* <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{headerShown: false}}
+      /> */}
+      <Stack.Screen
+        name="Basic"
+        component={BasicInfo}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Name"
+        component={NameScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Email"
+        component={EmailScreen}
+        options={{headerShown: false}}
+      />
+        <Stack.Screen
+        name="Password"
+        component={PasswordScreen}
+        options={{headerShown: false}}
+      />
+
+  <Stack.Screen
+        name="Birth"
+        component={BirthScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Location"
+        component={LocationScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Gender"
+        component={GenderScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Type"
+        component={TypeScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Dating"
+        component={DatingType}
+        options={{headerShown: false}}
+      />
+  <Stack.Screen
+        name="LookingFor"
+        component={LookingFor}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Hometown"
+        component={HomeTownScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Photos"
+        component={PhotoScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Prompts"
+        component={PromptsScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="ShowPrompts"
+        component={ShowPromptsScreen}
+        options={{headerShown: false}}
+      />
+         <Stack.Screen
+        name="PreFinal"
+        component={PreFinalScreen}
+        options={{headerShown: false}}
+      />
+        </Stack.Navigator>
+  );
+
   function MainStack() {
     return (
       <Stack.Navigator>
@@ -122,7 +224,7 @@ const StackNavigator = () => {
 
     return (
       <NavigationContainer>
-        <MainStack/>
+        <AuthStack/>
       </NavigationContainer>
     )
   }
